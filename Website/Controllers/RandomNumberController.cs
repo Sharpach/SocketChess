c656c0afc9f1ebf5c6dd6dfbff1cf0b849a05f7d
Nikita <kobzevnikita0@mail.ru>
2020-01-14 18:44:11 +0300

    adding website

---
+++ Website/Controllers/RandomNumberController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi;
+
+namespace Website.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomNumberController : ControllerBase
+    {
+	    // GET: api/RandomNumber
+	    [HttpGet]
+	    public int Get()
+	    {
+		    return RandomNumber.GetThreadRandom().Next(-10000, 50000);
+
+		}
+	 //   [HttpGet]
+		
+		
+
+
+    }
+}
